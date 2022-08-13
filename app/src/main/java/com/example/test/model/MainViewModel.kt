6936e3d6b0cf95f456a846f8c5a62b5b2a343b4e
xuguangdong <1728635490@qq.com>
2022-08-13 22:11:09 +0800

    ViewModel学习测试提交

---
+++ app/src/main/java/com/example/test/model/MainViewModel.kt
@@ -0,0 +1,26 @@
+package com.example.test.model
+
+import androidx.lifecycle.ViewModel
+
+/**
+ * @title:       标题
+ * @project:     Baymax
+ * @package:     com.example.test.model
+ * @class:       MainViewModel
+ * @description:
+ * <p>
+ * 类描述
+ * </p>
+ *
+ * @version:   1.0
+ * @createDate:   2022/8/13 20:47
+ * @Copyright (C) 2022 YSTEN
+ * @author:       xuguangdong
+ */
+class MainViewModel : ViewModel() {
+    /**
+     * 记数器数字
+     */
+    var count = 0
+
+}
--- app/src/main/java/com/example/test/view/MainActivity.kt
+++ app/src/main/java/com/example/test/view/MainActivity.kt
@@ -5,37 +5,65 @@ import android.content.Intent
 import android.graphics.Rect
 import android.os.Bundle
 import android.util.TypedValue
-import android.view.View
 import android.widget.TextView
+import androidx.fragment.app.FragmentActivity
+import androidx.lifecycle.ViewModelProviders
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.test.R
 import com.example.test.adapter.ButtonAdapter
 import com.example.test.model.Constant
-import com.example.test.service.MyService
+import com.example.test.model.MainViewModel
 
-class MainActivity : Activity() {
+class MainActivity : FragmentActivity() {
     private lateinit var mBtnRecyclerView: RecyclerView
-    private val mDataList = arrayListOf(Constant.补间动画, Constant.属性动画, Constant.内部文件存储
-            , Constant.外部文件存储, Constant.数据库存储, Constant.CONTENT_PROVIDER, Constant.SERVICE_TEST)
+
+    /**
+     * 点击按钮的总次数
+     */
+    private lateinit var clickCountTextView: TextView
+
+    /**
+     * 当前界面的ViewModel
+     */
+    private lateinit var mainViewModel: MainViewModel
+    private val mDataList = arrayListOf(
+        Constant.补间动画,
+        Constant.属性动画,
+        Constant.内部文件存储,
+        Constant.外部文件存储,
+        Constant.数据库存储,
+        Constant.CONTENT_PROVIDER,
+        Constant.SERVICE_TEST
+    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         mBtnRecyclerView = findViewById(R.id.btn_recyclerview)
+        clickCountTextView = findViewById(R.id.click_count)
+        mainViewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
         mBtnRecyclerView.adapter = ButtonAdapter(mDataList, this)
         mBtnRecyclerView.layoutManager = GridLayoutManager(this, 2)
         mBtnRecyclerView.addItemDecoration(object : RecyclerView.ItemDecoration() {
             override fun getItemOffsets(outRect: Rect, itemPosition: Int, parent: RecyclerView) {
-                outRect.bottom = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5f, resources.displayMetrics).toInt()
+                outRect.bottom = TypedValue.applyDimension(
+                    TypedValue.COMPLEX_UNIT_DIP,
+                    5f,
+                    resources.displayMetrics
+                ).toInt()
             }
         })
-        (mBtnRecyclerView.adapter as ButtonAdapter).onClickListener = ButtonAdapter.OnClickListener {
-            onClick(mDataList[it])
-        }
+        (mBtnRecyclerView.adapter as ButtonAdapter).onClickListener =
+            ButtonAdapter.OnClickListener {
+                onClick(mDataList[it])
+            }
+        refreshCount()
     }
 
     private fun onClick(buttonName: String) {
+        mainViewModel.count++
+        refreshCount()
         when (buttonName) {
             Constant.补间动画 -> startActivity(TweenActivity::class.java)
             Constant.内部文件存储 -> startActivity(InnerFirestoneActivity::class.java)
@@ -45,6 +73,11 @@ class MainActivity : Activity() {
             Constant.属性动画 -> startActivity(PropertyActivity::class.java)
             Constant.SERVICE_TEST -> startActivity(ServiceTest::class.java)
         }
+    }
+
+    private fun refreshCount() {
+        clickCountTextView.text =
+            resources.getString(R.string.current_click_count, mainViewModel.count)
     }
 
     private fun startActivity(targetCls: Class<*>) {
